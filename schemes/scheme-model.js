391733aad5bf7a23d3756b09f593839c2a444a6f
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-05 18:44:08 -0800

    set up join for findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,9 +3,22 @@ const db = require("../data/dbConfig");
 const model = {
     find: () => db.select('*').from('schemes'),
 
-    findById: id => db.select('*').from('schemes').where({ id: id }),
+    findById: id => {
+        const scheme = db.select('*').from('schemes').where({ id: id });
+        // should I be using try/catch here instead of if?
+        if (!scheme) { return null };
+        return scheme;
+    },
 
-    findSteps: id => db.select('step_number', 'scheme_name', 'instructions').where({ id: id }).orderBy('step_number'),
+    findSteps: schemeId => {
+        const steps = db('steps')
+            .join('schemes', 'steps.scheme_id', '=', 'schemes.id')
+            .select('steps.step_number', 'schemes.scheme_name', 'steps.instructions')
+            .where({ scheme_id: schemeId })
+            .orderBy('steps.step_number');
+        console.log(typeof steps);
+        return steps;
+    },
 
     add: scheme => {
         const newId = db.insert(scheme, 'id').into('schemes');
